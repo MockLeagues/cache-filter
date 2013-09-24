4abbce967c5e7672ed688faea25d28080087e4c8
Stefan Magnus Landrø <stefan.landro@gmail.com>
2013-09-24 18:03:10 +0200

    Added Expires header

--- src/main/java/no/bekk/boss/CacheHeaderFilter.java
+++ src/main/java/no/bekk/boss/CacheHeaderFilter.java
@@ -1,6 +1,9 @@
 package no.bekk.boss;
 
+import org.joda.time.DateTime;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CacheHeaderFilter implements Filter {
@@ -8,24 +11,20 @@ public class CacheHeaderFilter implements Filter {
     public static final String CACHE_CONTROL = "cache-control";
     public static final String TTL = "ttl"; // Seconds
 
+    private String cacheControl;
+    private int ttl;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+
         String cacheControl = filterConfig.getInitParameter(CACHE_CONTROL);
         String ttl = filterConfig.getInitParameter(TTL);
 
-        if (cacheControl == null || ttl == null) {
-            throw new ServletException("CacheHeaderFilter not configures correctly");
-        }
+        verifyConfiguration(cacheControl, ttl);
 
-        if (!cacheControl.equalsIgnoreCase("private") && !cacheControl.equalsIgnoreCase("public")) {
-            throw new ServletException("Specify either private or public for " + CACHE_CONTROL);
-        }
+        configureCacheControl(cacheControl);
 
-        try {
-            Long.parseLong(ttl);
-        } catch (NumberFormatException e) {
-            throw new ServletException("Specify a valid long for " + TTL, e);
-        }
+        configureTtl(ttl);
 
     }
 
@@ -33,10 +32,42 @@ public class CacheHeaderFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
 
+        DateTime dateTime = new DateTime();
+        DateTime dateTimePlusTtl = dateTime.plusSeconds(this.ttl);
+
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        res.setHeader("Cache-Control", this.cacheControl + ", max-age=" + this.ttl);
+        res.setDateHeader("Expires", dateTimePlusTtl.getMillis());
+
+        filterChain.doFilter(servletRequest, servletResponse);
 
     }
 
     @Override
     public void destroy() {
+
     }
+
+    private void verifyConfiguration(String cacheControl, String ttl) throws ServletException {
+        if (cacheControl == null || ttl == null) {
+            throw new ServletException("CacheHeaderFilter not configured correctly");
+        }
+    }
+
+    private void configureTtl(String ttl) throws ServletException {
+        try {
+            this.ttl = Integer.parseInt(ttl);
+        } catch (NumberFormatException e) {
+            throw new ServletException("Specify a valid long for " + TTL, e);
+        }
+    }
+
+    private void configureCacheControl(String cacheControl) throws ServletException {
+        if (!cacheControl.equalsIgnoreCase("private") && !cacheControl.equalsIgnoreCase("public")) {
+            throw new ServletException("Specify either private or public for " + CACHE_CONTROL);
+        }
+
+        this.cacheControl = cacheControl;
+    }
+
 }
--- src/test/java/no/bekk/boss/CacheHeaderFilterTest.java
+++ src/test/java/no/bekk/boss/CacheHeaderFilterTest.java
@@ -1,26 +1,45 @@
 package no.bekk.boss;
 
+import org.joda.time.DateTimeUtils;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 import org.mockito.Mock;
-
-import static no.bekk.boss.CacheHeaderFilter.CACHE_CONTROL;
-import static org.mockito.Mockito.*;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static no.bekk.boss.CacheHeaderFilter.CACHE_CONTROL;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CacheHeaderFilterTest {
 
     private CacheHeaderFilter filter;
 
-
     @Mock
     private FilterConfig filterConfigMock;
+
+    @Mock
+    private HttpServletRequest mockHttpServletRequest;
+
+    @Mock
+    private HttpServletResponse mockHttpServletResponse;
+
+    @Mock
+    private FilterChain mockFilterChain;
+
+    @BeforeClass
+    public static void setUpClass() {
+        DateTimeUtils.setCurrentMillisFixed(0);
+    }
 
     @Before
     public void setUp() {
@@ -29,16 +48,22 @@ public class CacheHeaderFilterTest {
 
     @Test(expected = ServletException.class)
     public void shouldFailIfConfigurationIsMissing() throws ServletException {
-       this.filter.init(filterConfigMock);
+        this.filter.init(filterConfigMock);
     }
 
     @Test
-    public void shouldNotFailIfConfigurationIsOK() throws ServletException {
+    public void shouldSetCorrectHttpHeaders() throws ServletException, IOException {
+        configureCacheHeaders();
+        this.filter.doFilter(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+        verify(mockHttpServletResponse).setHeader("Cache-Control", "public, max-age=600");
+        verify(mockHttpServletResponse).setDateHeader("Expires", 600 * 1000);
+        verify(mockFilterChain).doFilter(mockHttpServletRequest, mockHttpServletResponse);
+    }
+
+    private void configureCacheHeaders() throws ServletException {
         when(filterConfigMock.getInitParameter(CACHE_CONTROL)).thenReturn("public");
         when(filterConfigMock.getInitParameter(CacheHeaderFilter.TTL)).thenReturn("600");
         this.filter.init(filterConfigMock);
     }
-
-
 
 }
